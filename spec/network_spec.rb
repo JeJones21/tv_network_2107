0bfaea191e673643c9ccd6a95d60d682e049afad
Jessica Jones <daddehjones@gmail.com>
2021-07-21 12:21:56 -0400

    Create readable attributes test.

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Network do
 
     expect(nbc).to be_an_instance_of(Network)
   end
+
+  it "has readable attributes" do
+    nbc = Network.new("NBC")
+
+    expect(nbc.name).to eq("NBC")
+    expect(nbc.shows).to eq([])
+  end
 end
